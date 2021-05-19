6274f66b02468f2a77c4191c859aa95647880545
jessada <jessada.thutkawkorapin@gmail.com>
2021-05-20 05:22:56 +0700

    update JnK Autobot

--- JnK_Autobot.user.js
+++ JnK_Autobot.user.js
@@ -4,7 +4,12 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       JnK
-// @match        https://news.ycombinator.com
+// @include		   http://mousehuntgame.com/*
+// @include		   https://mousehuntgame.com/*
+// @include		   http://www.mousehuntgame.com/*
+// @include		   https://www.mousehuntgame.com/*
+// @include      http://www.mousehuntgame.com/camp.php*
+// @include      https://www.mousehuntgame.com/camp.php*
 // @grant        none
 // @run-at       document-end
 // @require      http://code.jquery.com/jquery-latest.js
