8bbd2bece10068cb51001c35e8b6462055152538
jessada <jessada.thutkawkorapin@gmail.com>
2021-05-20 04:25:41 +0700

    update JnK Autobot

---
+++ JnK_Autobot.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         JnK_Autobot
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       JnK
+// @match        https://news.ycombinator.com
+// @grant        none
+// @require http://code.jquery.com/jquery-latest.js
+// ==/UserScript==
+
+$(document).ready(function() {  $('.athing:odd').css('background-color', '#EEE');});
+
+(function() {
+    'use strict';
+
+    alert('Hello World !');
+})();
+
+
